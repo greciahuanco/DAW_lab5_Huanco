925a75475e7ee7609a39db9997ab19c1e93768ea
Grecia Huanco <grecia.huanco@tecsup.edu.pe>
2026-09-16 20:53:51 -0500

    Implementa API de productos con validaciones

---
+++ src/main/java/com/tecsup/controller/ProductoController.java
@@ -0,0 +1,89 @@
+package com.tecsup.controller;
+
+import java.util.List;
+
+import com.tecsup.dto.ProductoDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.tecsup.model.Producto;
+import com.tecsup.service.ProductoService;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> listar() {
+        return ResponseEntity.ok(service.listar());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> guardar(@Valid @RequestBody ProductoDTO dto) {
+
+        Producto p = new Producto();
+
+        p.setNombre(dto.getNombre());
+        p.setPrecio(dto.getPrecio());
+        p.setStock(dto.getStock());
+        p.setCategoria(dto.getCategoria());
+
+        Producto guardado = service.guardar(p);
+
+        return ResponseEntity.status(201).body(guardado);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<Producto>> buscarPorNombre(
+            @RequestParam String nombre) {
+
+        return ResponseEntity.ok(service.buscarPorNombre(nombre));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> obtener(@PathVariable Long id) {
+
+        Producto p = service.obtener(id);
+
+        if (p == null)
+            return ResponseEntity.status(404).body("Producto no encontrado");
+
+        return ResponseEntity.ok(p);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizar(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductoDTO dto) {
+
+        Producto existente = service.obtener(id);
+
+        if (existente == null)
+            return ResponseEntity.status(404).body("Producto no existe");
+
+        existente.setNombre(dto.getNombre());
+        existente.setPrecio(dto.getPrecio());
+        existente.setStock(dto.getStock());
+        existente.setCategoria(dto.getCategoria());
+
+        return ResponseEntity.ok(service.guardar(existente));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+
+        Producto p = service.obtener(id);
+
+        if (p == null)
+            return ResponseEntity.status(404).body("No existe");
+
+        service.eliminar(id);
+
+        return ResponseEntity.ok("Eliminado correctamente");
+    }
+}
